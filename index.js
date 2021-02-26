904ca7d459bca9eae78686973d388818f379689a
prahal <vprahaladhan@gmail.com>
2021-02-26 16:10:39 +1100

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerHTML = "Prahal is the champion";
+document.body.appendChild(newHeader);
